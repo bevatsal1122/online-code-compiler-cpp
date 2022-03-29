41f5d89a70b19bf2e4346f089307513eaf6178b8
Vatsal <bevatsal1122@gmail.com>
2022-03-29 12:13:43 +0530

    Integrated Post/Run Service

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,9 @@ const app = express();
 app.get("/", (req, res) => {
     return res.json({content: "Hello World!"});
 });
+app.post("/run", (req, res) => {
+    return res.json(req.body)
+});
 app.listen(4000, () => {
     console.log("Listening on Port 4000");
 });
